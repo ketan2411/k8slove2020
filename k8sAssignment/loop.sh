d7287aac078d61c6f6c2b4b3d8f26169aafb79fd
ketan sharma <41534044+ketan2411@users.noreply.github.com>
2020-05-23 02:28:08 +0530

    name updated

--- k8sAssignment/loop.sh
+++ k8sAssignment/loop.sh
@@ -2,6 +2,6 @@
 for i in range{1,2,3,4,5,6,7,8,9,10}
 do
   echo "Looping ... number $i"
-  kubectl run ashupods${i}  --image=alpine --restart=Never -- ping 8.8.8.8
+  kubectl run adhoc${i}  --image=alpine --restart=Never -- ping 8.8.8.8
 
 done
